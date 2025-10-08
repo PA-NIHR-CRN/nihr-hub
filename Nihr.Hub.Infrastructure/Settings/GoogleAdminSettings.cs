75f8e9d722be603c95407754fbf06bd8a93e1cac
Joseph Ellis <joseph.ellis@paconsulting.com>
2025-10-08 16:09:16 +0100

    chore: move gh token to env, fix warnings

--- Nihr.Hub.Infrastructure/Settings/GoogleAdminSettings.cs
+++ Nihr.Hub.Infrastructure/Settings/GoogleAdminSettings.cs
@@ -4,6 +4,6 @@ namespace Nihr.Hub.Infrastructure.Settings;
 
 public class GoogleAdminSettings
 {
-    [Required] public string KeyJson { get; set; }
-    [Required] public string AdminToImpersonate { get; set; }
+    [Required] public required string KeyJson { get; set; }
+    [Required] public required string AdminToImpersonate { get; set; }
 }
